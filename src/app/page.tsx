81d16886ca43e643dbebc3b747af8ba0546b9555
saiy2k <saiy2k@gmail.com>
2023-04-07 14:03:04 +0530

    Vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,43 @@
-import Image from 'next/image'
+'use client';
+
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { chakra, Box, Button, IconButton, Text } from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+import { useColorMode, useColorModeValue } from '@chakra-ui/system'
+import Image from 'next/image';
+
+import Logo from './images/logo.svg';
+import LogoWhite from './images/logo-white.svg';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box as='main' h='calc(100vh)' p={5}>
+      <Text as='h1'> Theme test page </Text>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <br/>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <IconButton 
+        aria-label='Toggle theme' 
+        onClick={() => { toggleColorMode(); console.log('theme: ', colorMode); }}
+        icon={colorMode === 'dark' ? <SunIcon />: <MoonIcon /> } />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <Button> Dummy button </Button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+      <br/>
+      <br/>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Image
+        src={useColorModeValue(Logo, LogoWhite)}
+        alt="Bull Bitcoin logo"
+        width="371" height="68"
+      />
+
+    </Box>
   )
 }
